e803fd6d1af0ec400930eb843589bf793ebad4c9
user <traveljaguar@gmail.com>
2023-06-12 21:04:47 +0900

    second

--- src/main/groovy/com/todoapp/todoapp/TodoController.java
+++ src/main/groovy/com/todoapp/todoapp/TodoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodoController {
 
     @GetMapping("/")
-    public String Todo() {
+    public String Todos() {
         return "To-do Application!";
     }
 }
