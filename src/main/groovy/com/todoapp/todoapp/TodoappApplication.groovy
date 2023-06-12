895962556edb12ec211ba09f777e2f8d364c47f6
user <traveljaguar@gmail.com>
2023-06-12 20:44:41 +0900

    initial

--- src/main/groovy/com/todoapp/todoapp/TodoappApplication.groovy
+++ src/main/groovy/com/todoapp/todoapp/TodoappApplication.groovy
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class TodoappApplication {
 
 	static void main(String[] args) {
-
 		SpringApplication.run(TodoappApplication, args)
 	}
 
